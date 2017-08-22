01038ac8551c5102c96dbdba505850ab34481001
Matias Llampa <hector.llampa@gmail.com>
2017-08-21 23:52:22 -0300

    Curso video 4 -  Menues

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,17 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { InicioPage } from '../pages/inicio/inicio';
+import { ContactoPage } from '../pages/contacto/contacto';
+import { AcercadePage } from '../pages/acercade/acercade';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    InicioPage,
+    ContactoPage,
+    AcercadePage
+
   ],
   imports: [
     BrowserModule,
@@ -19,7 +24,9 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    InicioPage,
+    ContactoPage,
+    AcercadePage
   ],
   providers: [
     StatusBar,
---
+++ src/pages/acercade/acercade.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { AcercadePage } from './acercade';
+
+@NgModule({
+  declarations: [
+    AcercadePage,
+  ],
+  imports: [
+    IonicPageModule.forChild(AcercadePage),
+  ],
+})
+export class AcercadePageModule {}
